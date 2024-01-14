748e604f531ec9d14636bfc589b1628a5769dd03
Andrey Ozornin <andrey.o@miro.com>
2024-01-14 19:47:27 +0100

    Initial commit

---
+++ eca.js
@@ -0,0 +1,89 @@
+const canvas = document.getElementById('eca')
+const ctx = canvas.getContext('2d');
+
+const width = canvas.width = window.innerWidth;
+const height = canvas.height = window.innerHeight;
+
+const PIXEL_SIZE = 4;
+
+const RULES = [
+    182, 99, 30, 154, 90, 195, 161
+]
+
+let RULE = 99;
+
+let initialState = (new Array(Math.ceil(width / PIXEL_SIZE))).fill(0);
+initialState[Math.floor(initialState.length / 2)] = 1;
+
+ctx.moveTo(0, 0);
+
+let generation = 0
+
+let timeout = null;
+
+function randomColor() {
+    return '#' + Math.random().toString(16).slice(2, 8);
+}
+
+let ticking = false;
+
+let color = randomColor();
+let bgColor = randomColor();
+
+document.body.addEventListener('click', () => {
+    color = randomColor();
+    bgColor = randomColor();
+    RULE = RULES[Math.floor(Math.random() * RULES.length)];
+    drawFrame();
+})
+
+function drawFrame() {
+    initialState = updateState(initialState, RULE);
+    let state = initialState;
+    for (let i = 0; i < height / PIXEL_SIZE; i++) {
+        drawGeneration(state, i, ctx);
+        state = updateState(state, RULE);
+    }
+    ticking = false;
+}
+
+drawFrame()
+
+const drawNextGeneration = () => {
+    if (timeout || ticking) return;
+    timeout = setTimeout(() => {
+        requestAnimationFrame(drawFrame)
+        ticking = true;
+        clearTimeout(timeout);
+        timeout = null;
+    }, 10)
+}
+
+document.body.addEventListener('mousemove', drawNextGeneration)
+
+function drawGeneration(state, i, ctx) {
+    for (let j = 0; j < state.length; j++) {
+        if (state[j] === 1) {
+            ctx.fillStyle = color;
+        } else {
+            ctx.fillStyle = bgColor;
+        }
+        ctx.fillRect(j * PIXEL_SIZE, i * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+    }
+}
+
+function updateState(state, ruleNumber) {
+    const newState = [];
+    for (let j = 0; j < state.length; j++) {
+        const left = state[j - 1] || 0;
+        const right = state[j + 1] || 0;
+        const center = state[j];
+
+        const rule = parseInt([left, center, right].join(''), 2); // 0-7
+        const newStateBit = ruleNumber.toString(2)[rule];
+        newState.push(parseInt(newStateBit, 10));
+    }
+    return newState;
+}
+
+
---
+++ scroll.js
@@ -0,0 +1,52 @@
+let lastKnownScrollPosition = 0;
+
+let screenWidth = window.innerWidth;
+let screenHeight = window.innerHeight;
+const mainContent = document.getElementById('main-content');
+const title = document.getElementById('title');
+const high = document.getElementsByClassName('title-high');
+const tech = document.getElementsByClassName('title-tech');
+const mess = document.getElementsByClassName('title-mess');
+
+function moveTitle(scrollPosition) {
+    const percentScrolled = Math.min(scrollPosition / screenHeight, 1);
+    mainContent.style.left = 50 - (50 * percentScrolled) + '%';
+    mainContent.style.height = `${Math.max(title.clientHeight, screenHeight - (screenHeight * percentScrolled))}px`;
+    mainContent.style.width = `${Math.max(title.clientWidth, (screenWidth - (screenWidth * percentScrolled)) / 2)}px`;
+    mainContent.style.top = `${Math.min(scrollPosition, screenHeight)}px`
+    mainContent.style.fontSize = `${Math.max(48, 160 - (percentScrolled * 140))}px`
+    
+    const horizontalShift = Math.sin(percentScrolled * 2 * Math.PI) * 50
+    const horizontalShift2 = Math.sin(percentScrolled * Math.PI) * 50
+    const verticalShift = Math.sin(horizontalShift)
+
+    high[0].style.marginLeft = `${-horizontalShift2 / 2}%`
+    high[1].style.marginLeft = `${-horizontalShift}%`
+    high[0].style.marginTop = `${verticalShift}%`
+    high[1].style.marginTop = `${verticalShift}%`
+    
+    mess[0].style.marginLeft = `${horizontalShift2}%`
+    mess[1].style.marginLeft = `${horizontalShift / 2}%`
+    mess[0].style.marginTop = `${-verticalShift}%`
+    mess[1].style.marginTop = `${-verticalShift}%`
+
+    if (scrollPosition > screenHeight) {
+        document.getElementById('tickets').style.boxShadow = `4px 4px 0px 0px ${color}`
+    } else {
+        document.getElementById('tickets').style.boxShadow = '4px 4px 0px 0px #000'
+    }
+}
+  
+document.addEventListener("scroll", () => {
+    lastKnownScrollPosition = window.scrollY;
+
+    if (!ticking) {
+        window.requestAnimationFrame(() => {
+            moveTitle(lastKnownScrollPosition);
+            ticking = false;
+        });
+
+        ticking = true;
+    }
+});
+  
